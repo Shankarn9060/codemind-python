e6cfef33d46865127ac64396f317f935544bea8c
Uma Shankar <97588329+Shankarn9060@users.noreply.github.com>
2022-10-12 23:44:38 +0530

    def count(a):     c=0     for i in range(1,a+1):         if a%i==0:             c+=1     return c a=int(input()) if count(a)==2:     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,11 @@
-n=int(input())
-c=0
-for i in range(1,n+1):
-    if n%i==0:
-        c+=1
-if c==2:
-    print('prime')
+def count(a):
+    c=0
+    for i in range(1,a+1):
+        if a%i==0:
+            c+=1
+    return c
+a=int(input())
+if count(a)==2:
+    print("prime")
 else:
-    print('not a prime')
+    print("not a prime")
